9c3f7e600100d2bd48076516434ad998972492a0
tony-g30 <tonyaudo@gmail.com>
2024-07-23 01:40:40 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// Multiply Variables
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+//Generate Random Integer
+const random = Math.floor(Math.random() * 10) + 1; // Generates random integer between 1 and 10
+console.log(random);
+
+//Calculate Remainder
+let num3 = 40;
+let num4 = 9;
+const mod = num3 % num4;
+console.log(mod); // Output-4
+
+// Challenge 4: Find Maximum Number
+const max = Math.max(1, 2, 3, 4, 5, 20);
+console.log(max); // Output-20
